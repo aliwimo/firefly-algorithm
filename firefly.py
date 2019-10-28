5850e9ba7abbd16d48db86dcb27f4fd29be717a9
aliwimo <aliwimo@gmail.com>
2019-10-28 23:55:45 +0300

    Last

--- firefly.py
+++ firefly.py
@@ -1,12 +1,14 @@
 import random
 import math
 import time
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
-MAX_GENERATION = 50
-POPULATION_SIZE = 90
+MAX_GENERATION = 10
+POPULATION_SIZE = 50
 DIMENSION_SIZE = 30
-ALPHA = 0.2
-BETA0 = 0
+ALPHA = 0.5
+BETA0 = 0.2
 GAMMA = 1.0
 TOP_LIMIT = 100
 DOWN_LIMIT = -100
@@ -14,6 +16,7 @@ DOWN_LIMIT = -100
 Location_Array = []
 Firefly_List = []
 Firefly_List0 = []
+Best = []
 
 
 class Firefly:
@@ -33,10 +36,19 @@ class Firefly:
     #     y = self.location[1]
     #     return ((x + (2 * y) - 7) ** 2) + (((2 * x) + y - 5) ** 2)
 
+    # def brightness(self):
+    #     brightness = 0
+    #     for index in range(DIMENSION_SIZE):
+    #         xi = self.location[index]
+    #         brightness += (xi ** 2) - 10 * math.cos(2 * math.pi * xi) - 10
+    #     return brightness
+
     def light_intensity(self, other):
+        # return self.brightness() * math.exp((-1) * GAMMA * (self.distance(other) ** 2))
         return self.brightness() * math.exp((-1) * GAMMA * self.distance(other))
 
     def attractiveness(self, other):
+        # return BETA0 * math.exp((-1) * GAMMA * (self.distance(other) ** 2))
         return BETA0 * math.exp((-1) * GAMMA * self.distance(other))
 
     def distance(self, other):
@@ -49,6 +61,7 @@ class Firefly:
     def update_location(self, other):
         for index in range(DIMENSION_SIZE):
             epsilon = random.random() - 0.5
+            # epsilon = 0.1
             rand = ALPHA * epsilon
             self.location[index] += self.attractiveness(other) * (other.location[index] - self.location[index]) + rand
             if self.location[index] > TOP_LIMIT:
@@ -69,12 +82,17 @@ def generate_fireflies():
 
 def run():
     for i in range(POPULATION_SIZE):
+        moved = False
         for j in range(POPULATION_SIZE):
-            if Firefly_List[i].brightness() > Firefly_List[j].light_intensity(Firefly_List[i]):
+            # if Firefly_List[i].brightness() > Firefly_List[j].light_intensity(Firefly_List[i]):
+            if Firefly_List[i].brightness() < Firefly_List[j].brightness():
                 new_firefly = Firefly(Firefly_List[i].location)
                 new_firefly.update_location(Firefly_List[j])
+                moved = True
                 if new_firefly.brightness() < Firefly_List[i].brightness():
                     Firefly_List[i] = new_firefly
+        if not moved:
+            Firefly_List[i].update_location(Firefly_List[j])
 
 
 def rank_fireflies():
@@ -83,13 +101,25 @@ def rank_fireflies():
         if Firefly_List[i].brightness() < best_firefly.brightness():
             best_firefly = Firefly_List[i]
     print(best_firefly.brightness())
+    Best.append(best_firefly.brightness())
 
+
+def show_plot():
+    plt.style.use('seaborn-whitegrid')
+    ax = plt.axes()
+    ax.plot(range(MAX_GENERATION), Best)
+    ax.set_ylabel('Values')
+    ax.set_xlabel('Generation')
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.show()
+    
 
 generate_fireflies()
-start_time = time.time()
 for gen in range(MAX_GENERATION):
+    start_time = time.time()
     print("Starting Generation: {}".format(gen))
     run()
+    elapsed_time = time.time() - start_time
     rank_fireflies()
-elapsed_time = time.time() - start_time
-print("Generation Time: {}".format(elapsed_time))
+    print("Generation Time: {}".format(elapsed_time))
+show_plot()
